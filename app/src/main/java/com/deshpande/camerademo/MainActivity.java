534381c46f20af45f7e37cf6bf6fb20f7a0fd6bd
guy301 <guy301@gmail.com>
2018-02-27 09:50:53 +0200

    27/2_v1

--- app/src/main/java/com/deshpande/camerademo/MainActivity.java
+++ app/src/main/java/com/deshpande/camerademo/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
     String datapath = "";
     Intent GalIntent, CamIntent, CropIntent;
 
-    // hi sap how f
+    // hi sap
     private Button mBtLaunchActivity;
 
     int i = 1;
